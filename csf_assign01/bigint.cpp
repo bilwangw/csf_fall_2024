40b728fbb763fbb6224de97c5751fc91319e56d0
Ryan Wu <rwu42@ugrad2.cs.jhu.edu>
2024-08-29 16:17:31 -0400

    basic constructors completed hw1

--- csf_assign01/bigint.cpp
+++ csf_assign01/bigint.cpp
@@ -1,18 +1,22 @@
 #include <cassert>
 #include "bigint.h"
+#include <algorithm>
 
 BigInt::BigInt()
   // TODO: initialize member variables
 {
-
+  data.push_back(0);
+  sign = false;
 }
 
 BigInt::BigInt(uint64_t val, bool negative)
   // TODO: initialize member variables
 {
+  sign = negative; 
+  data.push_back(val);
 }
 
-BigInt::BigInt(std::initializer_list<uint64_t> vals, bool negative)
+BigInt::BigInt(std::initializer_list<uint64_t> vals, bool negative): data(vals), sign(negative)
   // TODO: initialize member variables
 {
 }
@@ -20,6 +24,8 @@ BigInt::BigInt(std::initializer_list<uint64_t> vals, bool negative)
 BigInt::BigInt(const BigInt &other)
   // TODO: initialize member variables
 {
+  sign = other.sign;
+  data = other.data;
 }
 
 BigInt::~BigInt()
@@ -29,15 +35,39 @@ BigInt::~BigInt()
 BigInt &BigInt::operator=(const BigInt &rhs)
 {
   // TODO: implement
+  if (this != &rhs) {
+    data = rhs.data;
+    sign = rhs.sign;
+  }
+  return *this;
 }
 
 bool BigInt::is_negative() const
 {
   // TODO: implement
+  return sign;
 }
 
 const std::vector<uint64_t> &BigInt::get_bit_vector() const {
   // TODO: implement
+  std::vector<uint64_t>* ans = new std::vector<uint64_t>();
+  for (std::vector<uint64_t>::const_iterator it = data.begin(); it != data.end(); it++) {
+      uint64_t curr = *it;
+      // while (curr != 0) {
+      //   ans.push_back(curr % 2);
+      //   curr /= 2;
+      // }
+      while (curr) {
+        if (curr&1) {
+          (*ans).push_back(1);
+        } else {
+          (*ans).push_back(0);
+        }
+        curr >>=1;
+      }
+  }
+  std::reverse(ans->begin(), ans->end());
+  return *ans;
 }
 
 uint64_t BigInt::get_bits(unsigned index) const
@@ -88,7 +118,24 @@ int BigInt::compare(const BigInt &rhs) const
 
 std::string BigInt::to_hex() const
 {
+  std::string ans = "";
+  for (std::vector<uint64_t>::const_iterator it = data.begin(); it != data.end(); it++) {
+    uint64_t current = *it;
+    uint64_t counter = 1;
+    while (current != 0) {
+      int digit = current % counter;
+      if (digit > 9) {
+        ans += (digit-10) + 'a';
+      } else {
+        ans += digit;
+      }
+      counter *= 16;
+      current /= 16;
+    }
+  }
+  return ans;
   // TODO: implement
+  // divide 1, divide by 16, 
 }
 
 std::string BigInt::to_dec() const
