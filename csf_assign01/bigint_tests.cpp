30fab874626f231e82570877545051e8ac683110
Ryan Wu <rwu42@ugrad2.cs.jhu.edu>
2024-09-02 11:28:59 -0400

    first 4 tests passed

--- csf_assign01/bigint_tests.cpp
+++ csf_assign01/bigint_tests.cpp
@@ -73,7 +73,7 @@ int main(int argc, char **argv) {
   TEST(test_u64_ctor);
   TEST(test_initlist_ctor);
   TEST(test_copy_ctor);
-  //TEST(test_get_bits);
+  TEST(test_get_bits);
   // TEST(test_add_1);
   // TEST(test_add_2);
   // TEST(test_add_3);
