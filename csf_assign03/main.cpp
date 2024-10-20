c094a30969bf2e892c6282d42cc331ef7998cd81
Ryan Wu <rwu42@ugrad2.cs.jhu.edu>
2024-10-20 19:17:00 -0400

    completed error checking for input

--- csf_assign03/main.cpp
+++ csf_assign03/main.cpp
@@ -1,6 +1,10 @@
 #include <math.h>
 #include <iostream>
-
+#include <stdio.h>
+#include <vector>
+#include <cstdint>
+#include <cstdlib>
+#include <string>
 // ./csim 256 4 16 write-allocate write-back fifo < gcc.trace
 
 /* inputs
@@ -18,40 +22,69 @@ number of sets is not a power of 2
 block size is less than 4
 write-back and no-write-allocate were both specified
 */
+struct Slot {
+    uint32_t tag;
+    bool valid, dirty;
+    uint32_t load_ts, access_ts;
+};
 
+struct Set {
+    std::vector<Slot> slots;
+};
+
+struct Cache {
+    std::vector<Set> sets;
+};
+
+//block offset is memory address mod 2n (offset based on size of blocks in the memory (length of an element stored))
 int main (int argc, char *argv[])  {
     if (
-        ceil(log2(int(*argv[1]))) != floor(log2(int(*argv[1]))) // check if num sets is power of 2
+        ceil(log2(std::atoi(argv[1]))) != floor(log2(std::atoi(argv[1]))) // check if num sets is power of 2
     ) {
+        std::cout << argv[1] << "\n";
+        std::cout << std::atoi(argv[1]) << "\n";
         std::cerr << "Number of sets is not a power of 2\n";
         return 1;
     }
-    else if (ceil(log2(*argv[2])) != floor(log2(*argv[2]))) { // check if num of blocks is a power of 2
+    else if (ceil(log2(std::atoi(argv[2]))) != floor(log2(std::atoi(argv[2])))) { // check if num of blocks is a power of 2
         std::cerr << "Number of blocks is not a power of 2\n";
         return 1;
     }
-    else if (ceil(log2(*argv[3])) != floor(log2(*argv[3]))) { // check if block size is a power of 2
+    else if (ceil(log2(std::atoi(argv[3]))) != floor(log2(std::atoi(argv[3])))) { // check if block size is a power of 2
         std::cerr << "Block size is not a power of 2\n";
         return 1;
     }
-    else if (*argv[3] < 4) {  // check if block size is at least 4
+    else if (std::atoi(argv[3]) < 4) {  // check if block size is at least 4
         std::cerr << "Block size is not greater than or equal to 4\n";
     }
-    else if (
-        ((argv[4] != "write-allocate") && (argv[4] != "no-write-allocate")) // check if arguments are correct
-        || ((argv[5] != "write-through") && (argv[5] != "write-back")) // check if arguments are correct
-        || (argv[6] != "lru" && argv[6] != "fifo") // check if arguments are correct
-    ) {
-        std::cerr << "Invalid arguments\n";
-        return 1;
-    }
-    else if ((argv[5] == "write-back" && argv[4] == "no-write-allocate")) { // check condition
-        std::cerr << "write-back and no-write-allocate cannot be used at the same time\n";
-        return 1;
-    }
     else {
+        std::string arg4(argv[4]);
+        std::string arg5(argv[5]);
+        std::string arg6(argv[6]);
+        if (arg4 != "write-allocate" && arg4 != "no-write-allocate") {
+            std::cerr << "Invalid argument, must be write-allocate or no-write-allocate\n";
+            return 1;
+        }
+        if (arg5 != "write-through" && arg5 != "write-back") {
+            std::cerr << "Invalid argument, must be write-through or write-back\n";
+            return 1;
+        }
+        if (arg6 != "lru" && arg6 != "fifo") {
+            std::cerr << "Invalid argument, must be lru or fifo\n";
+            return 1;
+        }
+        if (arg5 == "write-back" && arg4 == "no-write-allocate") {
+            std::cerr << "write-back and no-write-allocate cannot be used at the same time\n";
+            return 1;
+        }
+        std::cout << argv[4] << "\n";
+        std::cout << argv[5] << "\n";
+        std::cout << argv[6] << "\n";
         
     }
+    // uint32_t num_sets = int(*argv[1]);
+    // uint32_t num_blocks = int(*argv[2]);
+    // uint32_t block_size = int(*argv[3]);
 
     return 0;
 }
