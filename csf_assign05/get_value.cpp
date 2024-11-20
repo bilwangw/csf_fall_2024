c3d1864a45a8dddd4e357a3b84c1f568a63ce831
Bill Wang <bwang89@jh.edu>
2024-11-19 20:21:59 -0500

    tweaks

--- csf_assign05/get_value.cpp
+++ csf_assign05/get_value.cpp
@@ -18,21 +18,32 @@ int main(int argc, char **argv)
   const char* hostname_c = hostname.c_str();
   const char* port_c = port.c_str();
   int fd = open_clientfd(hostname_c, port_c);
-
-  std::string login = "LOGIN " + username + "\n";
-  rio_writen(fd, login.c_str(), strlen(login.c_str()));
-  std::string get = "GET " + table + " " + key + "\n";
-  rio_writen(fd, get.c_str(), strlen(get.c_str()));
-  std::string top = "TOP\n";
-  rio_writen(fd, top.c_str(), strlen(top.c_str()));
   rio_t rio;
   rio_readinitb(&rio, fd);
   char buf[1000];
-  ssize_t n = rio_readnb(&rio, buf, sizeof(buf));
-  //while (n > 0) {
+  ssize_t n;
+
+  std::string login = "LOGIN " + username + "\n";
+  rio_writen(fd, login.c_str(), login.length());
+  n = rio_readnb(&rio, buf, sizeof(buf));
+  if (n > 0) {
     std::cout << buf;
-    //n = rio_readlineb(&rio, buf, sizeof(buf));
-  //}
+  }
+
+  std::string get = "GET " + table + " " + key + "\n";
+  rio_writen(fd, get.c_str(), get.length());
+  n = rio_readnb(&rio, buf, sizeof(buf));
+  if (n > 0) {
+    std::cout << buf;
+  }
+
+  std::string top = "TOP\n";
+  rio_writen(fd, top.c_str(), top.length());
+
+  n = rio_readnb(&rio, buf, sizeof(buf));
+  if (n > 0) {
+    std::cout << buf;
+  }
   
   std::string bye = "BYE\n";
   rio_writen(fd, bye.c_str(), bye.length());
--- csf_assign05/set_value.cpp
+++ csf_assign05/set_value.cpp
@@ -20,35 +20,35 @@ int main(int argc, char **argv)
   const char* port_c = port.c_str();
   int fd = open_clientfd(hostname_c, port_c);
 
+  // output for debugging
+  rio_t rio;
+  rio_readinitb(&rio, fd);
+  char buf[1000];
+  ssize_t n;
+
   std::string login = "LOGIN " + username + "\n";
-  rio_writen(fd, login.c_str(), strlen(login.c_str())+1);
-  //rio_writen(fd, "\n", 1);
+  rio_writen(fd, login.c_str(), login.length());
+  n = rio_readlineb(&rio, buf, sizeof(buf));
+  if (n > 0) {
+    std::cout << buf << "login\n";
+  }
   
   std::string push = "PUSH " + value + "\n";
   rio_writen(fd, push.c_str(), strlen(push.c_str()));
+  n = rio_readlineb(&rio, buf, sizeof(buf));
+  if (n > 0) {
+    std::cout << buf << "push\n";
+  }
 
   std::string set = "SET " + table + " " + key + "\n";
   rio_writen(fd, set.c_str(), strlen(set.c_str()));
-  //rio_writen(fd, "\n", 1);
-  // rio_t rio;
-  // rio_readinitb(&rio, fd);
-  // char buf[1000];
-  // ssize_t n = rio_readlineb(&rio, buf, sizeof(buf));
-  // if (n > 0) {
-  //   std::cout << buf;
-  // }
-  
-  //rio_writen(fd, "\n", 1);
-  // rio_t rio;
-  // rio_readinitb(&rio, fd);
-  // char buf[1000];
-  // ssize_t n = rio_readlineb(&rio, buf, sizeof(buf));
-  // if (n > 0) {
-  //   std::cout << buf;
-  // }
+  n = rio_readlineb(&rio, buf, sizeof(buf));
+  if (n > 0) {
+    std::cout << buf << "set\n";
+  }
+
   std::string bye = "BYE\n";
   rio_writen(fd, bye.c_str(), strlen(bye.c_str()));
-  //rio_writen(fd, "\n", 1);
 
   // const char* message = (login + push + set + bye).c_str();
   // rio_writen(fd, message, strlen(message));
