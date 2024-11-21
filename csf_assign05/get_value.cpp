2c5269b085e6529d64a99805c8d1adaac9431fb5
Bill Wang <bwang89@jh.edu>
2024-11-21 14:49:11 -0500

    more error handlign debugging

--- csf_assign05/get_value.cpp
+++ csf_assign05/get_value.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include "csapp.h"
 
+bool error_handling(ssize_t n, char* buf){
+  std::string output = buf;
+  // if(n < 0) {
+  //   std::cerr << "Error: read failed\n";
+  //   return 1;
+  // }
+  if(output.find("ERROR") != std::string::npos) { //error handling
+    std::cerr << "Error: " << output.substr(6,std::string::npos);
+    return 1;
+  }
+  else if(output.find("FAILED") != std::string::npos) { //error handling
+    std::cerr << "Error: " << output.substr(7,std::string::npos);
+    return 1;
+  }
+  else if(n < 0) {
+    std::cerr << "Error: read failed\n";
+    return 1;
+  }
+  return 0;
+}
+
 int main(int argc, char **argv)
 {
   if ( argc != 6 ) {
@@ -18,6 +39,9 @@ int main(int argc, char **argv)
   const char* hostname_c = hostname.c_str();
   const char* port_c = port.c_str();
   int fd = open_clientfd(hostname_c, port_c);
+  if (fd < 0) {
+    std::cerr << "Error: could not connect to server\n";
+  }
   rio_t rio;
   rio_readinitb(&rio, fd);
   char buf[1000];
@@ -27,54 +51,63 @@ int main(int argc, char **argv)
   std::string login = "LOGIN " + username + "\n";
   rio_writen(fd, login.data(), login.length());
   n = rio_readlineb(&rio, buf, sizeof(buf));
-  output = buf;
-  if(n <= 0) {
-    std::cerr << "Error: login failed\n";
-    return 1;
+  if(error_handling(n,buf)) {
+    return -1;
   }
-  if(output.find("ERROR") != std::string::npos) { //error handling
-    std::cerr << "Error: " << output.substr(6,std::string::npos);
-    return 1;
-  }
-  else if(output.find("FAILED") != std::string::npos) { //error handling
-    std::cerr << "Error: " << output.substr(7,std::string::npos);
-    return 1;
-  }
+  // output = buf;
+  // if(n < 0) {
+  //   std::cerr << "Error: login failed\n";
+  //   return -1;
+  // }
+  // if(output.find("ERROR") != std::string::npos) { //error handling
+  //   std::cerr << "Error: " << output.substr(6,std::string::npos);
+  //   return -1;
+  // }
+  // else if(output.find("FAILED") != std::string::npos) { //error handling
+  //   std::cerr << "Error: " << output.substr(7,std::string::npos);
+  //   return -1;
+  // }
 
 
   std::string get = "GET " + table + " " + key + "\n";
   rio_writen(fd, get.data(), get.length());
   n = rio_readlineb(&rio, buf, sizeof(buf));
-  output = buf;
-  if(n <= 0) {
-    std::cerr << "Error: get failed\n";
-    return 1;
+  if(error_handling(n,buf)) {
+    return -1;
   }
-  else if(output.find("ERROR") != std::string::npos) { //error handling
-    std::cerr << "Error: " << output.substr(6,std::string::npos);
-    return 1;
-  }
-  else if(output.find("FAILED") != std::string::npos) { //error handling
-    std::cerr << "Error: " << output.substr(7,std::string::npos);
-    return 1;
-  }
+  // output = buf;
+  // if(n < 0) {
+  //   std::cerr << "Error: get failed\n";
+  //   return -1;
+  // }
+  // else if(output.find("ERROR") != std::string::npos) { //error handling
+  //   std::cerr << "Error: " << output.substr(6,std::string::npos);
+  //   return -1;
+  // }
+  // else if(output.find("FAILED") != std::string::npos) { //error handling
+  //   std::cerr << "Error: " << output.substr(7,std::string::npos);
+  //   return -1;
+  // }
 
 
   std::string top = "TOP\n";
   rio_writen(fd, top.data(), top.length());
   n = rio_readlineb(&rio, buf, sizeof(buf));
   std::string data = buf;
-  if(n <= 0) {
-    std::cerr << "Error: top failed\n";
-    return 1;
-  }
-  else if(data.find("ERROR") != std::string::npos) { //error handling
-    std::cerr << "Error: " << data.substr(6,std::string::npos);
-    return 1;
-  }
-  else if(output.find("FAILED") != std::string::npos) { //error handling
-    std::cerr << "Error: " << data.substr(7,std::string::npos);
-    return 1;
+  // if(n < 0) {
+  //   std::cerr << "Error: top failed\n";
+  //   return -1;
+  // }
+  // else if(data.find("ERROR") != std::string::npos) { //error handling
+  //   std::cerr << "Error: " << data.substr(6,std::string::npos);
+  //   return -1;
+  // }
+  // else if(output.find("FAILED") != std::string::npos) { //error handling
+  //   std::cerr << "Error: " << data.substr(7,std::string::npos);
+  //   return -1;
+  // }
+  if(error_handling(n,buf)) {
+    return -1;
   }
   else if (n > 0) {
     std::cout << data.substr(5,std::string::npos); // get only the value from the line "DATA <value>"
@@ -83,19 +116,22 @@ int main(int argc, char **argv)
   std::string bye = "BYE\n";
   rio_writen(fd, bye.data(), bye.length());
   n = rio_readlineb(&rio, buf, sizeof(buf));
-  output = buf;
-  if(n <= 0) {
-    std::cerr << "Error: bye failed\n";
-    return 1;
+  if(error_handling(n,buf)) {
+    return -1;
   }
-  else if(output.find("ERROR") != std::string::npos) { //error handling
-    std::cerr << "Error: " << output.substr(6,std::string::npos);
-    return 1;
-  }
-  else if(output.find("FAILED") != std::string::npos) { //error handling
-    std::cerr << "Error: " << output.substr(7,std::string::npos);
-    return 1;
-  }
+  // output = buf;
+  // if(n < 0) {
+  //   std::cerr << "Error: bye failed\n";
+  //   return -1;
+  // }
+  // else if(output.find("ERROR") != std::string::npos) { //error handling
+  //   std::cerr << "Error: " << output.substr(6,std::string::npos);
+  //   return -1;
+  // }
+  // else if(output.find("FAILED") != std::string::npos) { //error handling
+  //   std::cerr << "Error: " << output.substr(7,std::string::npos);
+  //   return -1;
+  // }
 
   close(fd);
   return 0;
--- csf_assign05/set_value.cpp
+++ csf_assign05/set_value.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include "csapp.h"
 
+bool error_handling(ssize_t n, char* buf){
+  std::string output = buf;
+  if(n < 0) {
+    std::cerr << "Error: read failed\n";
+    return 1;
+  }
+  if(output.find("ERROR") != std::string::npos) { //error handling
+    std::cerr << "Error: " << output.substr(6,std::string::npos);
+    return 1;
+  }
+  else if(output.find("FAILED") != std::string::npos) { //error handling
+    std::cerr << "Error: " << output.substr(7,std::string::npos);
+    return 1;
+  }
+  return 0;
+}
+
 int main(int argc, char **argv)
 {
   if (argc != 7) {
@@ -19,6 +36,9 @@ int main(int argc, char **argv)
   const char* hostname_c = hostname.c_str();
   const char* port_c = port.c_str();
   int fd = open_clientfd(hostname_c, port_c);
+  if (fd < 0) {
+    std::cerr << "Error: could not connect to server\n";
+  }
 
   // output for debugging
   rio_t rio;
@@ -28,18 +48,30 @@ int main(int argc, char **argv)
   std::string login = "LOGIN " + username + "\n";
   rio_writen(fd, login.c_str(), strlen(login.c_str()));
   n = rio_readlineb(&rio, buf, sizeof(buf));
+  if(error_handling(n,buf)) {
+    return -1;
+  }
 
   std::string push = "PUSH " + value + "\n";
   rio_writen(fd, push.c_str(), strlen(push.c_str()));
   n = rio_readlineb(&rio, buf, sizeof(buf));
+  if(error_handling(n,buf)) {
+    return -1;
+  }
 
   std::string set = "SET " + table + " " + key + "\n";
   rio_writen(fd, set.c_str(), strlen(set.c_str()));
   n = rio_readlineb(&rio, buf, sizeof(buf));
+  if(error_handling(n,buf)) {
+    return -1;
+  }
 
   std::string bye = "BYE\n";
   rio_writen(fd, bye.data(), bye.length());
   n = rio_readlineb(&rio, buf, sizeof(buf));
+  if(error_handling(n,buf)) {
+    return -1;
+  }
 
 
   close(fd);
